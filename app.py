524a20001e5ed949de9315c5972a54e7036f93a8
Marcsi Papp <marcsisong@gmail.com>
2023-03-29 16:55:48 +0100

    creating questions from file

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from question import Question
 
-question_prompts = [
-    "What is a sole trader?\n(a) An individual in business, trading in his or her name, or under a trading name\n(b) A group of individuals working together as partners in business\n(c) An organisation, run by trustees, which uses its resources to fund charitable activities under its control\n\n",
-    "What colour are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
-    "What colour are strawberries?\n(a) Red\n(b) Purple\n(c) Orange\n\n"
-]
+import csv
 
-questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "c"),
-    Question(question_prompts[2], "a"),
-]
+with open(".\Multiple_choice_quiz\question.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
+    questions = []
+
+    for row in csv_reader:
+        prompt = row[0]
+        correct_answer = row[1]
+        wrong_answers = row[2:]
+        question = Question(prompt, correct_answer, wrong_answers)
+        questions.append(question)
 
 def run_test(questions):
     score = 0
     for question in questions:
         answer = input(question.prompt)
-        if answer == question.answer:
+        if answer == question.correct_answer:
             score += 1
             print("Correct answer! Keep going!")
         else:
             print("Wrong answer! Better luck next time!")
-            print("Correct answer: " + question.answer )
+            print("Correct answer: " + question.correct_answer)
     print("You got " + str(score) + "/" + str(len(questions)) + " correct")
-run_test(questions)
+#run_test(questions)
+print(questions[0].prompt + ":" + questions[0].correct_answer)
--- question.py
+++ question.py
@@ -1,4 +1,5 @@
 class Question:
-    def __init__(self, prompt, answer):
+    def __init__(self, prompt, correct_answer, wrong_answers):
         self.prompt = prompt
-        self.answer = answer
+        self.correct_answer = correct_answer
+        self.wrong_answers = wrong_answers
